dac74280d4839aa2b75490183e37e17a479ac470
vise07cs <vikram7cs@gmail.com>
2025-08-31 13:15:33 +0530

    T18: PostForm

---
+++ T18-Post/app.js
@@ -0,0 +1,16 @@
+// server.js
+const express = require("express");
+const app = express();
+const productRoutes = require("./routes/productRoutes");
+
+
+app.use(express.static("public"));
+app.use(express.json())
+
+app.use(express.json()); // for parsing JSON bodies
+app.use("/products", productRoutes);
+
+const PORT = 3020;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
